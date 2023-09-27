d049469f7d6fe51d7e5ca0fd5a40528b25cd2a6a
Cassiano Andrei <cschneider@inf.ufsm.br>
2023-09-27 17:40:40 -0300

    feat: Termos de uso

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class SiteConfig(models.Model):
+    termos_de_uso = models.TextField(default="Termos de uso")
+
+    class Meta:
+        verbose_name = 'Configurações do site'
+        verbose_name_plural = 'Configurações do site'
+
+    def __str__(self):
+        return 'Configurações do site'
